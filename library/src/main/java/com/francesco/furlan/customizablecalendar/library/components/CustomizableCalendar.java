c985a5c9a7618f7cc723974378e3aa5b697909f6
Francesco Furlan <francesco.furlan@molo17.com>
2017-07-04 11:59:04 +0200

    implemented CustomizableCalendar and CustomizableCalendarPresenter

--- library/src/main/java/com/francesco/furlan/customizablecalendar/library/components/CustomizableCalendar.java
+++ library/src/main/java/com/francesco/furlan/customizablecalendar/library/components/CustomizableCalendar.java
@@ -2,19 +2,28 @@ package com.francesco.furlan.customizablecalendar.library.components;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 
 import com.francesco.furlan.customizablecalendar.library.R;
+import com.francesco.furlan.customizablecalendar.library.interactors.ViewInteractor;
+import com.francesco.furlan.customizablecalendar.library.presenter.implementations.CustomizableCalendarPresenterImpl;
+import com.francesco.furlan.customizablecalendar.library.presenter.interfeaces.CustomizableCalendarPresenter;
+import com.francesco.furlan.customizablecalendar.library.view.CustomizableCalendarView;
+import com.google.gson.Gson;
 
+import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 /**
  * Created by francescofurlan on 23/06/17.
  */
 
-public class CustomizableCalendar extends LinearLayout {
+public class CustomizableCalendar extends LinearLayout implements CustomizableCalendarView {
 
     @InjectView(android.R.id.primary)
     HeaderView headerView;
@@ -27,6 +36,10 @@ public class CustomizableCalendar extends LinearLayout {
 
     @InjectView(android.R.id.content)
     CalendarRecyclerView calendarRecyclerView;
+
+    CustomizableCalendarPresenter presenter;
+
+    private int layoutResId = -1;
 
     public CustomizableCalendar(Context context) {
         this(context, null);
@@ -42,9 +55,53 @@ public class CustomizableCalendar extends LinearLayout {
     }
 
     private void init(Context context, AttributeSet attrs, int defStyleAttr) {
-        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CustomizableCalendar);
-        if (typedArray != null) {
-            typedArray.recycle();
+        if (presenter == null) {
+            TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CustomizableCalendar);
+            presenter = new CustomizableCalendarPresenterImpl();
+            presenter.setView(this);
+            layoutResId = R.layout.customizable_calendar;
+            if (typedArray != null) {
+                layoutResId = typedArray.getResourceId(R.styleable.CustomizableCalendar_layout, R.layout.customizable_calendar);
+                typedArray.recycle();
+            }
+
+            LayoutInflater.from(context).inflate(layoutResId, this);
+            ButterKnife.inject(this);
         }
+    }
+
+    public CustomizableCalendarPresenter getPresenter() {
+        return presenter;
+    }
+
+    @Override
+    public void injectViewInteractor(ViewInteractor viewInteractor) {
+        viewInteractor.onCustomizableCalendarBindView(this);
+        presenter.injectViewInteractor(viewInteractor);
+        headerView.injectPresenter(presenter);
+        calendarRecyclerView.injectPresenter(presenter);
+        subView.injectPresenter(presenter);
+        weekDaysView.injectPresenter(presenter);
+    }
+
+    @Override
+    public void injectPresenter(CustomizableCalendarPresenter presenter) {
+        this.presenter = presenter;
+        presenter.setView(this);
+    }
+
+    @Override
+    public void onCurrentMonthChanged(String month) {
+        subView.onMonthChanged(month);
+    }
+
+    @Override
+    public void refreshData() {
+
+    }
+
+    @Override
+    public void setLayoutResId(int layoutResId) {
+        this.layoutResId = layoutResId;
     }
 }

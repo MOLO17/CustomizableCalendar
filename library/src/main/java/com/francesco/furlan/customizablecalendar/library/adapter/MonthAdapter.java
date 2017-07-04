0a503091279b50369bd316ec054451c5a57c284a
Francesco Furlan <francesco.furlan@molo17.com>
2017-07-04 11:57:26 +0200

    made MonthAdapter customizable

--- library/src/main/java/com/francesco/furlan/customizablecalendar/library/adapter/MonthAdapter.java
+++ library/src/main/java/com/francesco/furlan/customizablecalendar/library/adapter/MonthAdapter.java
@@ -23,6 +23,7 @@ import org.joda.time.DateTime;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 import io.reactivex.disposables.CompositeDisposable;
 
@@ -97,65 +98,69 @@ public class MonthAdapter extends BaseAdapter implements MonthView {
 
         final CalendarItem currentItem = days.get(position);
 
-        final TextView dayView = (TextView) view.findViewById(android.R.id.title);
-        final View background = view.findViewById(android.R.id.background);
-        final View startSelectionView = view.findViewById(android.R.id.startSelectingText);
-        final View endSelectionView = view.findViewById(android.R.id.stopSelectingText);
-
-        startSelectionView.setVisibility(View.GONE);
-        endSelectionView.setVisibility(View.GONE);
-
-        if (currentItem == null) {
-            background.setBackground(null);
-            dayView.setText(null);
-        } else if (currentItem.compareTo(calendar.getFirstMonth().withMillisOfDay(0)) < 0) {
-            currentItem.setSelectable(false);
-            background.setBackground(null);
-            dayView.setTextColor(Color.BLACK);
-            dayView.setText(currentItem.getDayString());
-            dayView.setAlpha(0.6f);
+        if (viewInteractor != null && viewInteractor.hasImplementedMonthCellBinding()) {
+            viewInteractor.onMonthCellBindView(view);
         } else {
-            currentItem.setSelectable(true);
-            dayView.setAlpha(1f);
-            Integer backgroundResource = null;
-            if (firstSelectedDay != null) {
-                int startSelectedCompared = currentItem.compareTo(firstSelectedDay);
-                if (!multipleSelection) {
-                    if (startSelectedCompared == 0) {
-                        backgroundResource = R.drawable.empty_circle;
-                    }
-                } else if (startSelectedCompared == 0) {
-                    if (lastSelectedDay == null || lastSelectedDay.equals(currentItem.getDateTime())) {
-                        backgroundResource = R.drawable.circle;
-                    } else {
-                        backgroundResource = R.drawable.left_rounded_rectangle;
-                        endSelectionView.setVisibility(View.VISIBLE);
-                    }
-                } else if (startSelectedCompared > 0 && lastSelectedDay != null) {
-                    int endSelectedCompared = currentItem.compareTo(lastSelectedDay);
-                    if (endSelectedCompared == 0) {
-                        backgroundResource = R.drawable.right_rounded_rectangle;
-                        startSelectionView.setVisibility(View.VISIBLE);
-                    } else if (endSelectedCompared < 0) {
-                        backgroundResource = R.drawable.rectangle;
-                        startSelectionView.setVisibility(View.VISIBLE);
-                        endSelectionView.setVisibility(View.VISIBLE);
-                    }
-                }
-            }
+            final TextView dayView = (TextView) view.findViewById(android.R.id.title);
+            final View background = view.findViewById(android.R.id.background);
+            final View startSelectionView = view.findViewById(android.R.id.startSelectingText);
+            final View endSelectionView = view.findViewById(android.R.id.stopSelectingText);
 
-            int color = Color.BLACK;
-            if (backgroundResource != null) {
-                background.setBackgroundResource(backgroundResource);
-                if (multipleSelection) {
-                    color = Color.WHITE;
-                }
-            } else {
+            startSelectionView.setVisibility(View.GONE);
+            endSelectionView.setVisibility(View.GONE);
+
+            if (currentItem == null) {
                 background.setBackground(null);
-            }
+                dayView.setText(null);
+            } else if (currentItem.compareTo(calendar.getFirstMonth().withMillisOfDay(0)) < 0) {
+                currentItem.setSelectable(false);
+                background.setBackground(null);
+                dayView.setTextColor(Color.BLACK);
+                dayView.setText(currentItem.getDayString());
+                dayView.setAlpha(0.6f);
+            } else {
+                currentItem.setSelectable(true);
+                dayView.setAlpha(1f);
+                Integer backgroundResource = null;
+                if (firstSelectedDay != null) {
+                    int startSelectedCompared = currentItem.compareTo(firstSelectedDay);
+                    if (!multipleSelection) {
+                        if (startSelectedCompared == 0) {
+                            backgroundResource = R.drawable.empty_circle;
+                        }
+                    } else if (startSelectedCompared == 0) {
+                        if (lastSelectedDay == null || lastSelectedDay.equals(currentItem.getDateTime())) {
+                            backgroundResource = R.drawable.circle;
+                        } else {
+                            backgroundResource = R.drawable.left_rounded_rectangle;
+                            endSelectionView.setVisibility(View.VISIBLE);
+                        }
+                    } else if (startSelectedCompared > 0 && lastSelectedDay != null) {
+                        int endSelectedCompared = currentItem.compareTo(lastSelectedDay);
+                        if (endSelectedCompared == 0) {
+                            backgroundResource = R.drawable.right_rounded_rectangle;
+                            startSelectionView.setVisibility(View.VISIBLE);
+                        } else if (endSelectedCompared < 0) {
+                            backgroundResource = R.drawable.rectangle;
+                            startSelectionView.setVisibility(View.VISIBLE);
+                            endSelectionView.setVisibility(View.VISIBLE);
+                        }
+                    }
+                }
 
-            dayView.setTextColor(color);
-            dayView.setText(currentItem.getDayString());
+                int color = Color.BLACK;
+                if (backgroundResource != null) {
+                    background.setBackgroundResource(backgroundResource);
+                    if (multipleSelection) {
+                        color = Color.WHITE;
+                    }
+                } else {
+                    background.setBackground(null);
+                }
+
+                dayView.setTextColor(color);
+                dayView.setText(currentItem.getDayString());
+            }
         }
 
         return view;
@@ -182,27 +187,51 @@ public class MonthAdapter extends BaseAdapter implements MonthView {
     public void injectPresenter(CustomizableCalendarPresenter presenter) {
     }
 
+    /**
+     * Select the day specified if multiple selection mode is not enabled,
+     * otherwise adjust the ends of the selection:
+     * first end will be set if the day specified is before the first end;
+     * last end will be set if the day specified is after the last end;
+     *
+     * @param dateSelected a DateTime object that represents the day that
+     *                     should be selected
+     */
     @Override
     public void setSelected(DateTime dateSelected) {
-        if (!multipleSelection) {
-            notifyFirstSelectionUpdated(dateSelected);
-        } else {
-            if (firstSelectedDay != null) {
-                int startSelectedCompared = dateSelected.compareTo(firstSelectedDay);
-                if (startSelectedCompared < 0) {
+        if (viewInteractor != null && viewInteractor.hasImplementedSelection()) {
+            int itemSelected = viewInteractor.setSelected(multipleSelection, dateSelected);
+            switch (itemSelected) {
+                case 0:
                     notifyFirstSelectionUpdated(dateSelected);
-                } else if (lastSelectedDay != null) {
-                    int endSelectedCompared = dateSelected.compareTo(lastSelectedDay);
-                    if ((startSelectedCompared >= 0 && endSelectedCompared < 0) || endSelectedCompared > 0) {
+                    break;
+                case 1:
+                    notifyLastSelectionUpdated(dateSelected);
+                    break;
+                default:
+                    return;
+            }
+        } else {
+            if (!multipleSelection) {
+                notifyFirstSelectionUpdated(dateSelected);
+            } else {
+                if (firstSelectedDay != null) {
+                    int startSelectedCompared = dateSelected.compareTo(firstSelectedDay);
+                    if (startSelectedCompared < 0) {
+                        notifyFirstSelectionUpdated(dateSelected);
+                    } else if (lastSelectedDay != null) {
+                        int endSelectedCompared = dateSelected.compareTo(lastSelectedDay);
+                        if ((startSelectedCompared >= 0 && endSelectedCompared < 0) || endSelectedCompared > 0) {
+                            notifyLastSelectionUpdated(dateSelected);
+                        }
+                    } else {
                         notifyLastSelectionUpdated(dateSelected);
                     }
                 } else {
-                    notifyLastSelectionUpdated(dateSelected);
+                    notifyFirstSelectionUpdated(dateSelected);
                 }
-            } else {
-                notifyFirstSelectionUpdated(dateSelected);
             }
         }
+        notifyDataSetChanged();
     }
 
     private void notifyFirstSelectionUpdated(DateTime startSelected) {
@@ -221,27 +250,32 @@ public class MonthAdapter extends BaseAdapter implements MonthView {
         final int year = currentMonth.getYear();
         final int month = currentMonth.getMonthOfYear();
         final int firstDayOfMonth = currentMonth.getDayOfWeek() + 1;
-        final int lastDayOfMonth = DateUtils.getDaysInMonth(month - 1, year);
+        final int lastDayOfMonth = DateUtils.getDaysInMonth(month-1, year);
 
-        // default days calculation
-        if (firstDayOfMonth == firstDayOfWeek) {
-            empties = 0;
-        } else if (firstDayOfMonth < firstDayOfWeek) {
-            empties = Calendar.SATURDAY - (firstDayOfWeek - 1);
+        if (viewInteractor != null && viewInteractor.hasImplementedDayCalculation()) {
+            days = viewInteractor.calculateDays(year, month, firstDayOfMonth, lastDayOfMonth);
         } else {
-            empties = firstDayOfMonth - firstDayOfWeek;
-        }
-
-        int totDays = lastDayOfMonth + empties;
-        days = new ArrayList<>(totDays);
-
-        for (int day = 1, position = 1; position < totDays; position++) {
-            if (position > empties) {
-                days.add(new CalendarItem(day++, month, year));
+            // default days calculation
+            if (firstDayOfMonth == firstDayOfWeek) {
+                empties = 0;
+            } else if (firstDayOfMonth < firstDayOfWeek) {
+                empties = Calendar.SATURDAY - (firstDayOfWeek - 1);
             } else {
-                days.add(null);
+                empties = firstDayOfMonth - firstDayOfWeek;
+            }
+
+            int totDays = lastDayOfMonth + empties;
+            days = new ArrayList<>(totDays);
+
+            for (int day = 1, position = 1; position < totDays; position++) {
+                if (position > empties) {
+                    days.add(new CalendarItem(day++, month, year));
+                } else {
+                    days.add(null);
+                }
             }
         }
+
         notifyDataSetChanged();
     }
 

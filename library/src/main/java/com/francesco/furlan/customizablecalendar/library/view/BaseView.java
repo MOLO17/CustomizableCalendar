3aac6652a9a633dd4d75cfbd549c8682c8e70d2e
Francesco Furlan <francesco.furlan@molo17.com>
2017-07-04 11:00:23 +0200

    created base views interfaces structures

---
+++ library/src/main/java/com/francesco/furlan/customizablecalendar/library/view/BaseView.java
@@ -0,0 +1,8 @@
+package com.francesco.furlan.customizablecalendar.library.view;
+
+/**
+ * Created by francescofurlan on 23/06/17.
+ */
+
+public interface BaseView {
+}
---
+++ library/src/main/java/com/francesco/furlan/customizablecalendar/library/view/CustomizableCalendarView.java
@@ -0,0 +1,9 @@
+package com.francesco.furlan.customizablecalendar.library.view;
+
+/**
+ * Created by francescofurlan on 23/06/17.
+ */
+
+public interface CustomizableCalendarView extends BaseView {
+    void onCurrentMonthChanged(String month);
+}

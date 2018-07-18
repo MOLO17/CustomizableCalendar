22804ade8053f1096fccc8043abf25165742eccc
Rinat Shamshutdinov <rinat.shamshutdinov@simbirsoft.com>
2018-07-18 13:36:03 +0400

    Удалил из gradle joda-time

--- sample/src/test/java/com/molo17/customizablecalendar/sample/ExampleUnitTest.java
+++ sample/src/test/java/com/molo17/customizablecalendar/sample/ExampleUnitTest.java
@@ -1,10 +1,6 @@
 package com.molo17.customizablecalendar.sample;
 
-import org.joda.time.DateTime;
-import org.joda.time.Months;
 import org.junit.Test;
-import org.threeten.bp.LocalDate;
-import org.threeten.bp.Period;
 
 import static org.junit.Assert.assertEquals;
 
@@ -17,39 +13,5 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
-    }
-
-    @Test
-    public void checkDate() throws Exception {
-        LocalDate localDate1 = LocalDate.now();
-        LocalDate localDate2 = LocalDate.now();
-
-        assertEquals(localDate1.compareTo(localDate2), 0);
-
-        DateTime dateTime1 = DateTime.now();
-        DateTime dateTime2 = DateTime.now();
-
-        assertEquals(dateTime1.withMillisOfDay(0).compareTo(dateTime2.withMillisOfDay(0)), 0);
-
-        assertEquals(localDate1.withDayOfMonth(1).getDayOfMonth(), dateTime1.withDayOfMonth(1).withMillisOfDay(0).getDayOfMonth());
-
-        assertEquals(localDate1.getMonthValue(), dateTime1.getMonthOfYear());
-
-        assertEquals(localDate1.getDayOfWeek().getValue() + 1, dateTime1.getDayOfWeek() + 1);
-    }
-
-    @Test
-    public void checkDate1() throws Exception {
-        DateTime firstMonth = new DateTime().withMonthOfYear(1);
-        DateTime lastMonth = new DateTime();
-
-        int monthsBetweenCount = Months.monthsBetween(firstMonth, lastMonth).getMonths();
-
-        LocalDate localDate1 = LocalDate.now().withMonth(1);
-        LocalDate localDate2 = LocalDate.now();
-
-        int m2 = Period.between(localDate1, localDate2).getMonths();
-
-        assertEquals(monthsBetweenCount, m2);
     }
 }
